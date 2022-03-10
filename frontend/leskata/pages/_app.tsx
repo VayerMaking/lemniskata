d25f677ac80ad233d38c9757f4fbb7452d649765
Martin Vayer <martin.vayer@gmail.com>
2022-03-11 00:25:25 +0200

    [FE] add matine component library and test button

--- frontend/leskata/pages/_app.tsx
+++ frontend/leskata/pages/_app.tsx
@@ -1,8 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { MantineProvider, Button } from '@mantine/core';
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <MantineProvider theme={{ fontFamily: 'Open Sans' }}>
+      < Component {...pageProps} />
+      <Button>test button</Button>
+    </MantineProvider>
+  )
+
+
 }
 
 export default MyApp
