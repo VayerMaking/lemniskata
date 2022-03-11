f4bf04fb103a1865697603bc454f46550dbafa95
ZaharyMomchilov <zaharymomchilov@gmail.com>
2022-03-11 09:49:09 +0200

    [FE] Sidebar Init

---
+++ frontend/leskata/components/index/TypeBox.tsx
@@ -0,0 +1,9 @@
+import { Input } from '@mantine/core';
+
+export function TypeBox() {
+    return (
+      <Input
+        placeholder="Variable1"
+      />
+    );
+  }
--- frontend/leskata/pages/_app.tsx
+++ frontend/leskata/pages/_app.tsx
@@ -1,13 +1,12 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { MantineProvider, Button } from '@mantine/core';
-
+import { AppShell, Navbar, Header } from '@mantine/core';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <MantineProvider theme={{ fontFamily: 'Open Sans' }}>
       < Component {...pageProps} />
-      <Button>test button</Button>
     </MantineProvider>
   )
 
