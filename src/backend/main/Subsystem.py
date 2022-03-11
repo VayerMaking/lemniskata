82cbfabf8c72610627ce5791d764731adfc17f30
Kristiyan Stoimenov <kristoimenov@gmail.com>
2022-03-11 14:44:04 +0200

    [BE] Implement subsystem connections
    Different subsystems execute as different microservices.
    Their communication is performed through sockets.
    
    Signed-off-by: Kristiyan Stoimenov <kristoimenov@gmail.com>

---
+++ src/backend/main/Subsystem.py
@@ -0,0 +1,22 @@
+import socket
+
+class Subsystem:
+
+    def __init__(self, identifier, ip_and_port, num_sockets = 1):
+        self.identifier = identifier
+        self.ip_and_port = ip_and_port
+        self.sockets = {}
+
+    def connect_to(self, ss, conn_args=(socket.AF_INET, socket.SOCK_DGRAM)):
+        s = socket.socket(*conn_args)
+        s.connect(ss.ip_and_port)
+        self.sockets[ss.identifier] = s
+        print(f"{self.identifier} ({self.ip_and_port}) connected to {ss.identifier} ({ss.ip_and_port}) using '{s.getsockname()[0]}'")
+
+    def disconnect_from(self, ss_identifier):
+        s = self.sockets[ss_identifier]
+        print(f"{self.identifier} ({self.ip_and_port}) disconnected from {ss_identifier}")
+        s.close()
+        del self.sockets[ss_identifier]
+
+
