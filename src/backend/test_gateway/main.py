ac94b2a005ee980a6c17cd23a63ebdc77149ab2e
Martin Vayer <martin.vayer@gmail.com>
2022-03-11 13:58:01 +0200

    [BE][Docker]add basic inner between container communication tru sockets

---
+++ src/backend/test_gateway/main.py
@@ -0,0 +1,19 @@
+import socket
+import time
+HOST = 'gateway'
+PORT = 2001
+
+print("Server start at: %s/:%s" % (HOST, PORT))
+print("Waiting for connection...")
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen()
+conn, addr = s.accept()
+print('Connected by', addr)
+
+while True:
+    data = conn.recv(1024)
+    if not data:
+        break
+    print(f'Received from: {addr} :', data)
---
+++ src/backend/test_service/main.py
@@ -0,0 +1,10 @@
+import socket
+import time
+HOST = 'gateway'
+PORT = 2001
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((HOST, PORT))
+
+while True:
+    s.send(b'Hello, world. IPC success!')
