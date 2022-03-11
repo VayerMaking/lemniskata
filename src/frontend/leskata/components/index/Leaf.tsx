9e3c2bc011cbdd3fa5b45f187e0e56cf4ec92333
ZaharyMomchilov <zaharymomchilov@gmail.com>
2022-03-11 23:44:19 +0200

    [FE] Mars Map

--- src/frontend/leskata/components/index/Leaf.tsx
+++ src/frontend/leskata/components/index/Leaf.tsx
@@ -1,17 +1,19 @@
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+import { MapContainer, Marker, Popup, TileLayer, Rectangle } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 
 const Map = () => {
+  const center = [0, 0]
+  const rectangle = [
+    [0, 10],
+    [10, 20],
+  ]
+  const blackOptions = { color: 'black' }
   return (
     <MapContainer center={[0, 0]} zoom={3} scrollWheelZoom={false} style={{height: "1000px", width: "100%"}}>
       <TileLayer
-        url="https://trek.nasa.gov/tiles/Mars/EQ/Mars_MOLA_blend200ppx_HRSC_ClrShade_clon0dd_200mpp_lzw/1.0.0//default/default028mm/{x}/{y}/{z}.jpg"
+        url="https://trek.nasa.gov/tiles/Mars/EQ/Mars_MOLA_blend200ppx_HRSC_ClrShade_clon0dd_200mpp_lzw/1.0.0//default/default028mm/{z}/{y}/{x}.jpg"
       />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+      <Rectangle bounds={rectangle} pathOptions={blackOptions} />
     </MapContainer>
   )
 }
