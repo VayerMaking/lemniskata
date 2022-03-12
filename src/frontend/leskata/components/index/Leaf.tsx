13dded169b36c1b8aad0f3809f5bf115f3b418e1
ZaharyMomchilov <zaharymomchilov@gmail.com>
2022-03-12 22:07:55 +0200

    [FE] Ready for sending data to BE

--- src/frontend/leskata/components/index/Leaf.tsx
+++ src/frontend/leskata/components/index/Leaf.tsx
@@ -7,43 +7,96 @@ const center = [0,0];
 const data = require('../../json/poly.geo.json');
 
 const polygon = [
-  [0, 100],
-  [30, 100],
-  [20, 30],
+  [34.161818161230386, -85.95703125000001],
+  [-17.811456088564473, -60.46875000000001],
+  [21.779905342529645, -27.421875000000004]
 ]
+
 const purpleOptions = { color: 'purple' }
+
+const ShowPoly = ({ mapContainer, polygons }) => {
+  return polygons.map((poly, index) => {
+    return  <Circle key={index} uniceid={index} center={poly} pathOptions={purpleOptions} radius={1000000} />
+  })
+}
+
 
 const GetMousePos = ({ map }) => {
   // const [marker, setMarker] = useState([])
-  const [featured, setFeatured] = useState([]);
+  const [polygon, setPolygon] = useState([])
 
-
+  let polygonList = []
   useEffect(() => {
     if (!map) return;
+
     map.on('click', (e) => {
+      if (!e) return;
       // console.log(map.getPanes());
-      const { lat, lng } = e.latlng;
+      // let { lat, lng } = e.latlng;
       
+      // console.log(lat, lng);
+      
+      // polygonList.push(map.mouseEventToLatLng(e.originalEvent))
+      
+      setPolygon(pol => [...pol, [map.mouseEventToLatLng(e.originalEvent).lat, map.mouseEventToLatLng(e.originalEvent).lng]]);
+
       // console.log(e.target._layers[26]._tiles); 
       // console.log(e.latlng);
       // console.log(e.layerPoint);
-      let children = map.getPanes().mapPane.children[0].children[0].children[0].childNodes
-      let children2 = map.getPanes().mapPane.children[0].children[0].children[0].childNodes
+      // let x = map.getPanes().mapPane.children[0].children[0].children[0].childNodes[1]._leaflet_pos.x
+      // let y = map.getPanes().mapPane.children[0].children[0].children[0].childNodes[1]._leaflet_pos.y
+            
+      
+      // if(e.layerPoint.y !== map.layerPointToLatLng({x:x,y:y}).lat){
+      //   console.log("xd"+e.layerPoint.y);
+      //   console.log("dx"+map.layerPointToLatLng({x:x,y:y}).lat);
+        
+        
+      // }
+      // if(map.getPanes().mapPane.children[0].children[0].children[0]){
+      //   children = map.getPanes().mapPane.children[0].children[0].children[0].childNodes
+      // }
+      // else{
+      //   children = map.getPanes().mapPane.children[0].children[0].children[1].childNodes
+      // }
+      
+      let children = map.getPanes().mapPane.children[0].children[0].children[0].childNodes;
+      if(children.length == 0) return;
 
+      // if(map.getPanes().mapPane.children[0].children[0].children[0].childNodes == ''){
+      //   children = map.getPanes().mapPane.childNodes[0].childNodes[0].childNodes[1].childNodes[0]
+      // }
+      // else{
+      //   children = map.getPanes().mapPane.children[0].children[0].children[0].childNodes
+      // }
+      
+      
+      // console.log(map.containerPointToLatLng(map.getPanes().mapPane.children[0].children[0].children[0].childNodes[1]._leaflet_pos));
+      console.log(map.getPanes());
+      
       var regEx = /translate3d\((\d+)px, (\d+)px.*\)/gm
+      let json = {
+        data: []
+     };
       for(let k = 0; k < children.length; k++){
-        const {x,y} = children[k]._leaflet_pos
+        let {x,y} = children[k]._leaflet_pos
         
         // console.log(128 - (Math.abs(y) - Math.abs(e.layerPoint.y)));
-        if(256 - Math.abs(y - e.layerPoint.y) > 0 && 256 - Math.abs(x - e.layerPoint.x) > 0 ){
-              console.log(children[k].src);
-              // console.log(children[k].style.transform);
-              //console.log(getMatches(children[k].style.transform, myRegEx, 2))
-              // const match = regEx.exec(children[k].style.transform)
-              //console.log(children[k].style.transform);
-              console.log();
+        if(128 - Math.abs(e.layerPoint.y - y) > 0 && 128 - Math.abs(e.layerPoint.x - x) > 0 ){
+          console.log(x,y);
+          //console.log(e.layerPoint.x, e.layerPoint.y);  
+          // console.log(`https://trek.nasa.gov/tiles/Mars/EQ/Mars_MOLA_blend200ppx_HRSC_ClrShade_clon0dd_200mpp_lzw/1.0.0//default/default028mm/3/${Number(picX) + 1}/${Number(picY) + 1}.jpg`);
+          console.log(children[k].src );
+          json.data.push({x: x, y:y, url:children[k].src})            
+            // console.log(map.layerPointToContainerPoint({x:x,y:y}))
+            // console.log(map.containerPointToLatLng({x:map.layerPointToContainerPoint({x:x,y:y}).x,y: map.layerPointToContainerPoint({x:x,y:y}).y}))
+
+              // console.log(map.layerPointToLatLng({x:x,y:y}).lat);
+              // json.data.push({x: map.layerPointToLatLng({x:x,y:y}).lat, y:map.layerPointToLatLng({x:x,y:y}).lng, url: children[k].src });
         }
       }
+
+     console.log(json);
       //console.log(e.layerPoint.divideBy(256));
       //console.log(featured);
       
@@ -56,13 +109,15 @@ const GetMousePos = ({ map }) => {
       // console.log(data.geometries[0].coordinates);
     })
 
-    for(let i=0; i < data.lenght; i++){
-      console.log(data);
-      
-    }
+    
 
   }, [map]);
-  return null
+  return polygon.length > 0  ? ( 
+    <ShowPoly
+      mapContainer={map}
+      polygons={polygon} />
+  )
+    : null
 }
 
 
@@ -94,11 +149,12 @@ const Map = () => {
     <MapContainer whenCreated={setMap}  center={[0, 0]} zoom={3} scrollWheelZoom={true} style={{height: "1000px", width: "100%"}}>
       <TileLayer url="https://trek.nasa.gov/tiles/Mars/EQ/Mars_MOLA_blend200ppx_HRSC_ClrShade_clon0dd_200mpp_lzw/1.0.0//default/default028mm/{z}/{y}/{x}.jpg"
       />
-      {/* <Polygon pathOptions={purpleOptions} positions={data.geometries[1].coordinates} /> */}
+      {/* <Polygon pathOptions={purpleOptions} positions={polygon} /> */}
         <GetMousePos map={map} />
         {/* {poly} */}
     </MapContainer>
   )
 }
 
+// data.geometries[1].coordinates
 export default Map
--- src/frontend/leskata/components/pic_of_day/PicOfTheDayComponents.tsx
+++ src/frontend/leskata/components/pic_of_day/PicOfTheDayComponents.tsx
@@ -1,14 +1,16 @@
 import { Image } from '@mantine/core';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function PicOfTheDayComponent() {
     const [currPicUrl, setCurrPicURL] = useState<string>();
 
+    
     async function fetchImageOfTheDay() {
         const res = await fetch('https://api.nasa.gov/planetary/apod?api_key=6BaJSTfT8toGrjcyJwImEvSg2aofL9bOVYpVYL8d');
         const json = res.json()
         setCurrPicURL(json['url'])
     }
+    useEffect(() => {fetchImageOfTheDay()}), []
 
     console.log("alt text: ", currPicUrl.split('/')[-1].split('.')[0]);
 
