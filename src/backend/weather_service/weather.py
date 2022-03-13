1a6068f8d082f8807b785db62423035e9622a0c4
Martin Vayer <martin.vayer@gmail.com>
2022-03-13 05:44:43 +0200

    [BE][Docker] fix connections and requirements

--- src/backend/weather_service/weather.py
+++ src/backend/weather_service/weather.py
@@ -12,12 +12,13 @@ class WeatherEvaluater:
 
     def check_weather(self, longitude, latitude, day):
         weather_data = requests \
-            .get("http://wheather_api:6969/",
+            .get("http://weather_api:60/",
                  params={"longitude": longitude, "latitude": latitude}).json()
         day_weather = weather_data["sol_keys"][int(day)]
 
         valid_weather = [1 if (day_weather["AT"] in self.__weather_temperature_boundaries) else 0,
-                         1 if (day_weather["PRE"] in self.__weather_pressure_boundaries) else 0,
+                         1 if (
+                             day_weather["PRE"] in self.__weather_pressure_boundaries) else 0,
                          1 if (day_weather["HWS"] in self.__weather_wind_boundaries) else 0]
 
         return dict(
