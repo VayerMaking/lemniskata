e05f93f172baec71f6431f059de6a117b7d23eff
Kristiyan Stoimenov <kristoimenov@gmail.com>
2022-03-13 03:36:36 +0200

    [BE][Topo] Execute topo evaluator through service
    Signed-off-by: Kristiyan Stoimenov <kristoimenov@gmail.com>

--- src/backend/height_service/main.py
+++ src/backend/height_service/main.py
@@ -5,33 +5,51 @@ from PIL import Image
 import requests
 from io import BytesIO
 
+from topo import *
 
-class MyHandler(BaseHTTPRequestHandler):
-    def _set_headers(self):
+def b():
+    with open('harry_json.json', 'r') as f:
+        return f.read()
+
+def api_url(x, y, z):
+        return f'https://trek.nasa.gov/tiles/Mars/EQ/Mars_MOLA_blend200ppx_HRSC_ClrShade_clon0dd_200mpp_lzw/1.0.0//default/default028mm/{z}/{y}/{x}.jpg'
+
+
+def multistep(bd):
+    urls_to_imgs = {}
+    for j in json.loads(bd): # FIXME: load?
+        url = api_url(j['x'], j['y'], j['z'])
+        print(url)
+        urls_to_imgs[url] = Image.open(BytesIO(requests.get(url).content))
+        urls_to_imgs[url].show()
+    topo_evaluator = TopoEvaluator(list(urls_to_imgs.values()))
+    total = topo_evaluator.eval()
+    return total
+
+
+class TopoService(BaseHTTPRequestHandler):
+
+    def __api_url(x, y, z):
+        return f'https://trek.nasa.gov/tiles/Mars/EQ/Mars_MOLA_blend200ppx_HRSC_ClrShade_clon0dd_200mpp_lzw/1.0.0//default/default028mm/{z}/{y}/{x}.jpg'
+
+    def __set_headers(self):
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
         self.end_headers()
 
+
+    def __body(self):
+        return self.rfile.read(int(self.headers['Content-Length']))
+
+
     def do_POST(self):
-        self._set_headers()
-
-        if self.path == '/generate/map/height':
-            content_length = int(self.headers['Content-Length'])
-            body = self.rfile.read(content_length)
-            json_array = json.loads(body)
-            urls_array = []
-            for point in json_array:
-                x_coordinate = point['x']
-                y_coordinate = point['y']
-                z_coordinate = point['z']
-                url = f'https://trek.nasa.gov/tiles/Mars/EQ/Mars_MOLA_blend200ppx_HRSC_ClrShade_clon0dd_200mpp_lzw/1.0.0//default/default028mm/{z_coordinate}/{y_coordinate}/{x_coordinate}.jpg'
-                urls_array.append(url)
-                img_res = requests.get(url)
-                img = Image.open(BytesIO(img_res.content))
-                # TODO call algorithm
-
-            self.wfile.write(json.dumps(urls_array).encode("utf-8"))
+        if self.path != '/generate/map/height':
+            return
+        self.__set_headers()
+        multistep(self.__body())
+        # FIXME: self.wfile.write(list(urls_to_imgs.keys()).encode("utf-8"))
 
 
-httpd = socketserver.TCPServer(("height_service", 6972), MyHandler)
-httpd.serve_forever()
+if __name__ == "__main__":
+    httpd = socketserver.TCPServer(("height_service", 6972), TopoService)
+    httpd.serve_forever()
