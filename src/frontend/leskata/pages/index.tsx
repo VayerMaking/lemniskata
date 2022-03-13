aae831946c8157b728ed4a8b9983415ec31ee308
ZaharyMomchilov <zaharymomchilov@gmail.com>
2022-03-13 06:33:01 +0200

    [FE] weatherApi

--- src/frontend/leskata/pages/index.tsx
+++ src/frontend/leskata/pages/index.tsx
@@ -12,18 +12,12 @@ function Main() {
   const theme = useMantineTheme();
   const position = [51.505, -0.09]
 
-  const Map = dynamic(() => import('../components/index/Leaf'), { ssr: false });
-
   return (
     <div>
     
       <Box>
-      {/* <Map provider={mapTiler} height={300} defaultCenter={[5, 5]} defaultZoom={5}>
-      <ZoomControl />
-      </Map> */}
         {/* <HomePage /> */}
-        {/* <Nav/> */}
-        <Map/>
+        <Nav/>
       </Box>
     </div>
   );
