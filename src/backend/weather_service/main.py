73dd7c59b3ce64d8e53561bcc221a45c77db1dcd
Tinchev <st8tinchev@gmail.com>
2022-03-13 04:56:33 +0200

    BE - weather api which iterate through different map blocks

--- src/backend/weather_service/main.py
+++ src/backend/weather_service/main.py
@@ -16,24 +16,25 @@ class MyHandler(BaseHTTPRequestHandler):
         self.send_header('Content-type', 'application/json')
         self.end_headers()
 
-    def do_POST(self):
+    def do_GET(self):
         self._set_headers()
 
         if self.path == '/generate/map/weather':
             content_length = int(self.headers['Content-Length'])
             body = self.rfile.read(content_length)
-            weather_eval(json.loads(body)['longitude'], json.loads(body)['latitude'],
-                         0 if json.loads(body)['day'] is None else int(json.loads(body)['day']))
+
+            result = dict()
+            for value in json.loads(body):
+                longitude = value['x']
+                latitude = value['y']
+                try:
+                    day = value[2]
+                except IndexError:
+                    day = 0
+                result[[longitude, latitude]] = weather_eval(longitude, latitude, day)
+
             self.wfile.write(body)
 
 
 httpd = socketserver.TCPServer(("weather_service", 6971), MyHandler)
 httpd.serve_forever()
-
-# @app.route("/api/weather", methods=["GET"])
-# def fetch_weather_data():
-# longitude = request.args.get('longitude')
-# latitude = request.args.get('latitude')
-# day = 0 if request.args.get('day') is None else int(request.args.get('day'))
-
-# return weather.check_weather(longitude, latitude, day)
